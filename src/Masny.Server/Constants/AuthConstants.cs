22d0f125b245b957b8159fb2cddcf0d29ee8fe60
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-07-26 13:30:25 +0300

    feat: add JsonWebToken auth

---
+++ src/Masny.Server/Constants/AuthConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Masny.Server.Constants
+{
+    public static class AuthConstants
+    {
+        public const string Audiance = "https://localhost:5001";
+        public const string Issuer = Audiance;
+        public const string Secret = "MySecretKey";
+    }
+}
